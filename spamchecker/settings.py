724e04a8d30238f2a15f1e3c2951216a940bf80b
Venkateswararaobhimasingi <srinu19773@gmail.com>
2024-09-21 21:03:13 +0530

    ture

--- spamchecker/settings.py
+++ spamchecker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8*8uq7=&@z$0s997xfdh3(g@=8j@*sq6kqdqy1w$r3ukj64-14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
