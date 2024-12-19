45421ba504c9c27e75891f8e6e43669447a5e14e
Venkateswararaobhimasingi <141164246+Venkateswararaobhimasingi@users.noreply.github.com>
2024-12-19 12:57:00 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages 
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+from django.contrib.auth import logout as auth_logout
+
 
 def register(request):
     if request.method == 'POST':
@@ -41,3 +43,9 @@ def profile(request):
         'p_form':p_form
     }
     return render(request,'users/profile.html',context)
+
+
+
+def logout(request):
+    auth_logout(request)  # Use Django's built-in logout function
+    return render(request, 'users/logout.html')
